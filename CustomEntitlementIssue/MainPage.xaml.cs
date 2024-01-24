38fcf6f490def578e981a69055b9aedcd5cbbe66
Allan Ritchie <allan.ritchie@gmail.com>
2024-01-24 11:21:33 -0500

    Initial commit

---
+++ CustomEntitlementIssue/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomEntitlementIssue;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void OnCounterClicked(object sender, EventArgs e)
+	{
+		try
+		{
+			await SecureStorage.Default.SetAsync("Test", "Test Value");
+            CounterBtn.Text = "SUCCESS";
+        }
+		catch (Exception ex)
+		{
+			CounterBtn.Text = ex.ToString();
+		}
+	}
+}
